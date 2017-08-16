daf69c59dc9739fef6b800e703fd4cfe9a69f135
Aaron Ofengender <aofengen@gmail.com>
2017-08-16 14:09:26 -0400

    added variables file

---
+++ 0.02_variables.js
@@ -0,0 +1,52 @@
+//variables
+//something that can store data
+
+// var name; //declaration of variable
+// name = "Madyson"; //initialization of variable
+// console.log(typeof(name));
+// console.log(name);
+// var lastName = "Ward";//declaration and initialization of variable
+// console.log(name.length);
+// console.log(name.toUpperCase());
+// console.log(lastName);
+// //Challenge: Create two variables 'firstName' and 'lastName' and concat them together into a third variable called fullName
+// var firstName = "Aaron";
+// lastName = "Ofengender";
+// var fullName = firstName + " " + lastName;
+// console.log(fullName);
+
+//BRONZE CHALLENGE
+var playerNumber = 10;
+var teamNumber = 12;
+var position = 3;
+var wins = 16;
+var losses = 4;
+
+console.log("BRONZE");
+console.log("My number is " + playerNumber);
+console.log("My team number is " + teamNumber);
+console.log("My poistion is " + position + ", first base.");
+console.log("My team has " + wins + " wins this season");
+console.log("My team has " + losses + " losses this season");
+console.log("");
+//SILVER CHALLENGE
+
+console.log("SILVER");
+var myNumber = 0;
+myNumber += 10; //myNumber = (myNumber + 10);
+console.log(myNumber);
+myNumber -= 1;  //myNumber = (myNumber - 1);
+console.log(myNumber);
+myNumber *= 9;  //myNumber = (myNumber * 9);
+console.log(myNumber);
+myNumber /= 7;  //myNumber = (myNumber/7);
+console.log(myNumber);
+console.log("");
+
+//GOLD CHALLENGE - Lifetime Supply
+var currentAge = 29;
+var maxAge = 100;
+var amountPerDay = 3;
+var lifetimeSupply = ((maxAge-currentAge)*365);
+console.log("GOLD");
+console.log("You will need " + lifetimeSupply + " cookies to last you until the ripe old age of " + maxAge + ".");
