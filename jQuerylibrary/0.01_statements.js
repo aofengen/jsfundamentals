e7eaccb7b28a03d693d38fd4c2d86a8ff0719014
Aaron Ofengender <aofengen@gmail.com>
2017-08-22 16:30:20 -0400

    adding jQuery statements file

---
+++ jQuerylibrary/0.01_statements.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+	$("#example").css("background-color", "yellow");
+	$("#example").css("border", "solid", "2px", "black");//Challenge: add a border around <p>#example
+});
